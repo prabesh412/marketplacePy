36c92f4a510ef462d563620065975d22fa77a6ba
Prabesh Uprety <prabeshuprety1@gmail.com>
2024-02-11 17:56:07 +0545

    add: nginx

--- config/settings/base.py
+++ config/settings/base.py
@@ -241,7 +241,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""prabesh """, "prabeshuprety1@gmail.com")]
+ADMINS = [("""prabesh """, "prabesh@mail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 # https://cookiecutter-django.readthedocs.io/en/latest/settings.html#other-environment-settings
