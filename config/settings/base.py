a318db1af678a5e4e69bf24c4574af7cb25f8a7a
Prabesh Uprety <prabeshuprety1@gmail.com>
2024-02-19 15:39:15 +0545

    origin config fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -342,7 +342,10 @@ REST_FRAMEWORK = {
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
-CORS_URLS_REGEX = r"^/api/.*$"
+CORS_ALLOWED_ORIGINS = [
+    'https://doshrodeal.com',
+    'https://www.doshrodeal.com',
+]
 
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
