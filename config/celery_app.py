d2b1313b024560ef3d813e27cd42a8df2e4f43c0
Prabesh Uprety <prabeshuprety1@gmail.com>
2024-02-24 21:21:13 +0545

    new page added

--- config/celery_app.py
+++ config/celery_app.py
@@ -29,7 +29,7 @@ app.conf.beat_schedule = {
     },
     "get_all_categories": {
         "task": "doshro_bazar.listings.tasks.get_all_categories",
-        "schedule": 60, 
+        "schedule": 6000, 
     },
 
 }
